8587dfed55b80708df674be2cf95ff74882224c6
sad8787 <43324098+sad8787@users.noreply.github.com>
2021-07-31 03:19:35 +0300

    Add files via upload

---
+++ Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Tests:Test
+    {
+
+       public List<Test> values { get; set; }
+    }
+}
